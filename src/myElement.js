e90b8488e7c09859383845d4c7a45fd88858c6f9
KortexDevLinux <cacortesl01@gmail.com>
2023-01-20 16:11:13 -0500

    Se crea el componente web my-component-test y se agrega al html.

--- src/myElement.js
+++ src/myElement.js
@@ -1,8 +1,19 @@
+const template = document.createElement("div");
+template.innerHTML = `
+    <p>Me gusta wow</p>
+`;
+
 //HTMLElement es la web Api que nos ayuda a contruir un "custom element"
 class myElement extends HTMLElement{
     constructor(){
         super();
-        console.log("Aflición")
+        this.p = document.createElement("p");
+    }
+
+    connectedCallback(){
+        this.p.textContent = "Hola world of warcraft";
+        this.appendChild(this.p);
+        this.appendChild(template);
     }
 }
 
